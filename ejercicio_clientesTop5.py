78952de7e76fce6baca309b31728e281ba8c1e91
Alex Rayon <alex.rayon@brainandcode.tech>
2026-08-29 09:13:48 +0000

    feat: añado primer ejercicio klin

--- ejercicio_clientesTop5.py
+++ ejercicio_clientesTop5.py
@@ -1,10 +1,8 @@
 
-# Calculo el gasto medio por transaccion de cada cliente
-df["gasto_medio"] = df["gasto_total"] / df["total_transacciones"]
-
 # Muestro los 5 clientes con mayor gasto
+
 top_clientes = df[
-    ["nombre", "apellidos", "total_transacciones", "gasto_total", "gasto_medio"]
+    ["nombre", "apellidos", "total_transacciones", "gasto_total"]
 ].head(5)
 
 print(top_clientes)
